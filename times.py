5c073c4194447cbefb278d01eaf61634bad4855c
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2024-12-31 21:42:51 +0000

    Add files via upload

--- times.py
+++ times.py
@@ -1,23 +1,7 @@
 from datetime import datetime
-then = datetime(2025, 1, 1, 0, 0, 0) 
+then = datetime(2026, 1, 1, 0, 0, 0) 
 now = datetime.now() # Now
 print("\033[43;30m\n")
 duration = now - then # For build-in functions
-duration_in_s = duration.total_seconds() 
-message = """
-  _   _  ____   ____  _    _   _   _  _   _  _   _ 
- | | | |/ __ \\ / __ \\| |  | | | \\ | || \\ | || \\ | |
- | |_| | |  | | |  | | |  | | |  \\| ||  \\| ||  \\| |
- |  _  | |  | | |  | | |  | | | . ` || . ` || . ` |
- | | | | |__| | |__| | |__| | | |\\  || |\\  || |\\  |
- |_| |_|\\____/ \\____/ \\____/  |_| \\_||_| \\_||_| \\_|
-
-       _   _ ______ _      __     ______  _    _ 
-      | \\ | ||  ___| |     \\ \\   / / __ \\| |  | |
-      |  \\| || |_  | |      \\ \\_/ / |  | | |  | |
-      | . ` ||  _| | |       \\   /| |  | | |  | |
-      | |\\  || |___| |____    | | | |__| | |__| |
-      |_| \\_\\_____/|______|   |_|  \\____/ \\____/ 
-"""
-print(message)
+duration_in_s =int( duration.total_seconds()/(60*60*24))
 print(duration_in_s)
