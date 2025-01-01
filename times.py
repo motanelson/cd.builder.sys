d96e73651b9ff62ede168b2f8f5937d621780fdd
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-01-01 12:10:44 +0000

    Add files via upload

--- times.py
+++ times.py
@@ -3,5 +3,5 @@ then = datetime(2026, 1, 1, 0, 0, 0)
 now = datetime.now() # Now
 print("\033[43;30m\n")
 duration = now - then # For build-in functions
-duration_in_s =int( duration.total_seconds()/(60*60*24*7))
+duration_in_s =int( duration.total_seconds()/(60*60*24*(30.5)))
 print(duration_in_s)
