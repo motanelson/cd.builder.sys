7b2daa8d38be3abe7cadfe2799319124c76853a0
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-01-08 15:36:59 +0000

    Add files via upload

--- mysys.s
+++ mysys.s
@@ -25,17 +25,67 @@ loop1:
     jnz loop1
 
     pop ds
-    mov bp, msg     ; Ponteiro para a mensagem
+
+jmp _main
+nop
+oem             db      'MY OEM  '
+bsector         dw      200h
+scluster        db      1h 
+rsector         dw      1h
+tfat            db      2h
+rent            dw      0e0h
+tsectors        dw      0b40h
+media           db      0f0h 
+sfat            dw      9h
+strak           dw      12h
+head            dw      2h
+hidden          dd      0h
+large           dd      0h
+drive           db      0h
+flag            db      0h
+sig             db      29h
+vol             dd      0ffffffffh
+label           db      'MY LABEL    '
+id              db      'FAT12   '
+;--------------------------------------------------------
+eess            dw      0
+ees1            dw      0
+_main:
+    mov bx,area1
+    mov al,0x70
+    mov ch,0
+    mov cl,1
+    mov dh,0
+    mov ah,2
+    mov dl,128
+;int load sectores into memory
+    int 13h
+
+
+
+
+
+    mov ax, area1     ; Ponteiro para a mensagem
+    add ax,33600
+    mov bp,ax
+prints:
+    push bp
     mov bl,0x60
     mov bh,0
     mov dh,0
     mov dl,0
-    mov cx,1000
+    mov cx,1600
     mov ah,0x13
     mov al,0x1
     int 0x10
-    
+    mov ax,0
+    int 0x16
+    pop ax
+    add ax,1600
+    mov bp,ax
+    jmp prints
 .hang:
     cli             ; Desabilita interrupções
     hlt             ; Entra em estado de espera
 
+area1:
